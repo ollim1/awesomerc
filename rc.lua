fcac74c919a450fcb24dd4052343dccc97ebafe4
Olli Määttä <ollimaatta@gmail.com>
2020-02-05 14:11:23 +0200

    preserve client to tag mappings when monitor configuration changed

--- rc.lua
+++ rc.lua
@@ -634,7 +634,7 @@ awful.rules.rules = {
     -- EDIT: custom rules
     -- { rule = { class = "mpv" },
     -- properties = { floating = true }},
-    { rule = { class = "TelegramDesktop" },
+    { rule_any = { class = { "TelegramDesktop", "Audacious" }},
         screen = awful.screen.focused,
         properties = { sticky = true, skip_taskbar = true }
     }
@@ -718,20 +718,59 @@ client.connect_signal("property::floating", set_sizehints)
 client.connect_signal("property::name", sort_client)
 
 -- enable restoration of client layout on change of screen configuration
+-- Save and restore tags, when monitor setup is changed
+local naughty = require("naughty")
+local tag_store = {}
 tag.connect_signal("request::screen", function(t)
-    for s in screen do
-        if s ~= t.screen and
-            s.geometry.x == t.screen.geometry.x and
-            s.geometry.y == t.screen.geometry.y and
-            s.geometry.width == t.screen.geometry.width and
-            s.geometry.height == t.screen.geometry.height then
-            local t2 = awful.tag.find_by_name(s, t.name)
-            if t2 then
-                t:swap(t2)
-            else
-                t.screen = s
-            end
-            return
-        end
+  local fallback_tag = nil
+
+  -- find tag with same name on any other screen
+  for other_screen in screen do
+    if other_screen ~= t.screen then
+      fallback_tag = awful.tag.find_by_name(other_screen, t.name)
+      if fallback_tag ~= nil then
+        break
+      end
     end
+  end
+
+  -- no tag with same name exists, chose random one
+  if fallback_tag == nil then
+    fallback_tag = awful.tag.find_fallback()
+  end
+
+  if not (fallback_tag == nil) then
+    local output = next(t.screen.outputs)
+
+    if tag_store[output] == nil then
+      tag_store[output] = {}
+    end
+
+    clients = t:clients()
+    tag_store[output][t.name] = clients
+
+    for _, c in ipairs(clients) do
+      c:move_to_tag(fallback_tag)
+    end
+  end
 end)
+
+screen.connect_signal("added", function(s)
+  local output = next(s.outputs)
+  naughty.notify({ text = output .. " Connected" })
+
+  tags = tag_store[output]
+  if not (tags == nil) then
+    naughty.notify({ text = "Restoring Tags" })
+
+    for _, tag in ipairs(s.tags) do
+      clients = tags[tag.name]
+      if not (clients == nil) then
+        for _, client in ipairs(clients) do
+          client:move_to_tag(tag)
+        end
+      end
+    end
+  end
+end)
+
