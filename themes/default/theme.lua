1a497dfafd315f8e5e88998cc8c46b2f0f37e7c3
Olli Määttä <ollimaatta@gmail.com>
2020-02-06 13:28:46 +0200

    make inactive clients fully translucent in task list

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -30,6 +30,7 @@ theme.border_normal = "#000000"
 theme.border_focus  = "#808080"
 theme.border_marked = "#91231c"
 
+theme.tasklist_bg_normal = "#00000000"
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
